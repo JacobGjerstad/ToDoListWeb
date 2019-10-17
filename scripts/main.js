4f3e4d66275a56e23a1b938cd9fd14464ccb4156
JACOB Gjerstad <gjerstad0715@students.cptc.edu>
2019-10-17 14:53:52 -0700

    Created ToDoItem class

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/**
+ * Represents a single task on a todo list
+ */
+var ToDoItem = /** @class */ (function () {
+    function ToDoItem(task) {
+        this.title = task;
+    }
+    return ToDoItem;
+}());
---
+++ typescripts/main.ts
@@ -0,0 +1,12 @@
+/**
+ * Represents a single task on a todo list
+ */
+class ToDoItem{
+    title:string;
+    deadline:Date;
+    isCompleted:boolean;
+
+    constructor(task:string){
+        this.title = task;
+    }
+}
